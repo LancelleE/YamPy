e596d5a4273080efbdf67d87c224f2f73a8cd6be
etiennelancelle <elancelle.pro@gmail.com>
2023-12-19 01:38:58 +0100

    edit main

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from src.Player import Player
 from src.Dice import Dice
 
-J1 = Player('etienne')
-d = Dice()
-print(f"""
-      Le J1 est {J1.playerName}.\n
-      Les dés ont donné : {d.D1}, {d.D2}, {d.D3}, {d.D4}, {d.D4}
-""")
-d.reroll('D1')
-print(d.D1)
+if __name__ == '__main__':
+    print('coucou')
